d206ae628e27666d8ed7736069cba76a76f15920
Eleazar Morelli <gemorelli@gmail.com>
2015-03-04 15:40:15 -0300

    Card retrieval

--- www/js/buttons-controller.js
+++ www/js/buttons-controller.js
@@ -1,34 +1,6 @@
 criticalRoller.controller('ButtonsController', function($scope, $http) {
-  $scope.drawCriticalSlashing = function() {
-
-  };
-
-  $scope.drawCriticalPiercing = function() {
-
-  };
-
-  $scope.drawCriticalBludgeoning = function() {
-
-  };
-
-  $scope.drawCriticalMagic = function() {
-
-  };
-
-  $scope.drawFumbleMelee = function() {
-
-  };
-
-  $scope.drawFumbleRanged = function() {
-
-  };
-
-  $scope.drawFumbleNatural = function() {
-
-  };
-
-  $scope.drawFumbleMagic = function() {
-
+  $scope.draw = function(rollType, attackType) {
+    $scope.drawnCards.push($scope.criticalSlashing[1]);
   };
 
 });
--- www/js/cards-controller.js
+++ www/js/cards-controller.js
@@ -1,38 +1,36 @@
 criticalRoller.controller('CardsController', function($scope, $http) {
 
-  $scope.currentCard = {};
+  $scope.getData = function(rollType, attackType) {
+    var url = "data/" + rollType + "-" + attackType + ".json";
+    $http.get(url).success(function(data) {
+      $scope.cards[rollType][attackType] = data;
+    });
+  }
 
   $scope.drawnCards = [];
 
-  $http.get("data/critical-slashing.json").success(function(data) {
-    $scope.criticalSlashing = data;
-  });
+  $scope.cards = {
+    "critical": {
+      "slashing": [],
+      "piercing": [],
+      "bludgeoning": [],
+      "magic": []
+    },
+    "fumble": {
+      "melee": [],
+      "ranged": [],
+      "natural": [],
+      "magic": [],
+    }
+  };
 
-  $http.get("data/critical-piercing.json").success(function(data) {
-    $scope.criticalPiercing = data;
-  });
+  $scope.getData("critical", "slashing");
+  $scope.getData("critical", "piercing");
+  $scope.getData("critical", "bludgeoning");
+  $scope.getData("critical", "magic");
+  $scope.getData("fumble", "melee");
+  $scope.getData("fumble", "ranged");
+  $scope.getData("fumble", "natural");
+  $scope.getData("fumble", "magic");
 
-  $http.get("data/critical-bludgeoning.json").success(function(data) {
-    $scope.criticalBludgeoning = data;
-  });
-
-  $http.get("data/critical-magic.json").success(function(data) {
-    $scope.criticalMagic = data;
-  });
-
-  $http.get("data/fumble-melee.json").success(function(data) {
-    $scope.fumbleMelee = data;
-  });
-
-  $http.get("data/fumble-ranged.json").success(function(data) {
-    $scope.fumbleRanged = data;
-  });
-
-  $http.get("data/fumble-Natural.json").success(function(data) {
-    $scope.fumbleNatural = data;
-  });
-
-  $http.get("data/fumble-magic.json").success(function(data) {
-    $scope.fumbleMagic = data;
-  });
 });
