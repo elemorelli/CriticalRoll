837024d7d39f1fdbf8f0ff93285c2c44442616c0
Eleazar Morelli <gemorelli@gmail.com>
2015-03-04 20:21:19 -0300

    Card Drawing

--- www/js/buttons-controller.js
+++
@@ -1,6 +0,0 @@
-criticalRoller.controller('ButtonsController', function($scope, $http) {
-  $scope.draw = function(rollType, attackType) {
-    $scope.drawnCards.push($scope.criticalSlashing[1]);
-  };
-
-});
--- www/js/cards-controller.js
+++ www/js/cards-controller.js
@@ -1,15 +1,22 @@
-criticalRoller.controller('CardsController', function($scope, $http) {
+criticalRoller.controller('CardsController', function($scope, $http, $ionicScrollDelegate) {
 
   $scope.getData = function(rollType, attackType) {
     var url = "data/" + rollType + "-" + attackType + ".json";
     $http.get(url).success(function(data) {
-      $scope.cards[rollType][attackType] = data;
+      $scope.cardsData[rollType][attackType] = data;
     });
-  }
+  };
 
-  $scope.drawnCards = [];
+  $scope.transformClass = function(classToTransform) {
+    switch (classToTransform) {
+      case "critical":
+        return "balanced";
+      case "fumble":
+        return "assertive";
+    }
+  };
 
-  $scope.cards = {
+  $scope.cardsData = {
     "critical": {
       "slashing": [],
       "piercing": [],
@@ -32,5 +39,28 @@ criticalRoller.controller('CardsController', function($scope, $http) {
   $scope.getData("fumble", "ranged");
   $scope.getData("fumble", "natural");
   $scope.getData("fumble", "magic");
+
+  $scope.drawnCards = [];
+
+  $scope.draw = function(rollType, attackType) {
+
+    var cardArray = $scope.cardsData[rollType][attackType];
+
+    var randomIndex = Math.floor(Math.random() * cardArray.length);
+
+    cardDrawn = cardArray[randomIndex];
+    cardDrawn["rollType"] = $scope.transformClass(rollType);
+    cardDrawn["attackType"] = attackType;
+
+    $scope.drawnCards.push(cardDrawn);
+
+    $ionicScrollDelegate.scrollBottom(true);
+  };
+
+  $scope.remove = function(index) {
+    $scope.drawnCards.splice(index, 1);
+  }
+
+
 
 });
