3d1cd7bcfcc63ef1f1f09312591fbce5fb65dc37
Eleazar Morelli <gemorelli@gmail.com>
2015-03-24 16:42:17 -0300

    Version update

--- prepare-apk.bat
+++ prepare-apk.bat
@@ -2,8 +2,8 @@
 del CriticalRoll*.apk
 call ionic build
 call cordova build --release android
-copy platforms\android\ant-build\CordovaApp-release-unsigned.apk CriticalRoll-unsigned.apk
+copy platforms\android\ant-build\MainActivity-release-unsigned.apk CriticalRoll-unsigned.apk
 call "%java_home%\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore CriticalRoll-unsigned.apk gemorelli
-call "%android_home%\build-tools\21.1.2\zipalign.exe" -v 4 CriticalRoll-unsigned.apk CriticalRoll.apk
+call "%android_home%\build-tools\22.0.0\zipalign.exe" -v 4 CriticalRoll-unsigned.apk CriticalRoll.apk
 del CriticalRoll-unsigned.apk
 @echo on
