de4fdb1da08d05cbc96de3b1494ae576730e8d02
Eleazar Morelli <gemorelli@gmail.com>
2015-04-22 21:48:43 -0300

    LanguageController and Menu translation

---
+++ www/js/language-controller.js
@@ -0,0 +1,44 @@
+CriticalRoll.controller('LanguageController', function($scope, $translate) {
+  $scope.changeLanguage = function() {
+    $translate.use($scope.selected.id);
+  };
+
+  $scope.getLanguage = function(id) {
+    for (language in $scope.languages) {
+      var lang = $scope.languages[language];
+      if (lang.id == id)
+        return lang;
+    }
+  };
+
+  $scope.languages = [{
+    "id": "en",
+    "label": "English"
+  }, {
+    "id": "es",
+    "label": "Español"
+  }]
+
+  $scope.selected = $scope.getLanguage($translate.use());
+
+});
+
+CriticalRoll.config(['$translateProvider', function($translateProvider) {
+  $translateProvider.translations('en', {
+    'HELP': 'Help!',
+    'ABOUT': 'About',
+    'LICENSE': 'License',
+    'CONTRIBUTE': 'How to contribute',
+    'LANGUAGE': 'Language'
+  });
+
+  $translateProvider.translations('es', {
+    'HELP': 'Ayuda!',
+    'ABOUT': 'Acerca',
+    'LICENSE': 'Licencia',
+    'CONTRIBUTE': 'Como ayudar',
+    'LANGUAGE': 'Idioma'
+  });
+
+  $translateProvider.preferredLanguage('es');
+}]);
