449e8336e1b08dcd29db2d3a24ae335effa9dc83
Eleazar Morelli <gemorelli@gmail.com>
2015-05-13 01:32:57 -0300

    Version update

--- prepare-apk.bat
+++ prepare-apk.bat
@@ -4,5 +4,6 @@ call ionic build --release android
 call cordova build --release android
 copy platforms\android\ant-build\MainActivity-release-unsigned.apk MainActivity-unsigned.apk
 call "%java_home%\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore MainActivity-unsigned.apk gemorelli
+call "%android_home%\build-tools\22.0.1\zipalign.exe" -v 4 MainActivity-unsigned.apk MainActivity.apk
 del MainActivity-unsigned.apk
 @echo on
