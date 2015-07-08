3968ab2d79656cd9b6accecd8f6156ea6f89d9d2
Eleazar Morelli <gemorelli@gmail.com>
2015-07-08 00:18:30 -0300

    Improving the Hack for WP

--- www/js/app.js
+++ www/js/app.js
@@ -14,30 +14,8 @@ var openLink = function (href) {
 	return false;
 };
 
-ionic.Platform.isIE = function () {
-	return ionic.Platform.ua.toLowerCase().indexOf('trident') > -1;
-}
+// Horrible hack for WindowsPhone: https://github.com/driftyco/ionic/issues/2150
 
-// Horrible Hack to prevent double event triggering on IE. See https://github.com/driftyco/ionic/issues/2885
-if (ionic.Platform.isIE()) {
-	angular.module('ionic').factory('$ionicNgClick', ['$parse', '$timeout', function ($parse, $timeout) {
-		return function (scope, element, clickExpr) {
-			var clickHandler = angular.isFunction(clickExpr) ? clickExpr : $parse(clickExpr);
-
-			element.on('click', function (event) {
-				scope.$apply(function () {
-					if (scope.clicktimer) return; // Second call
-					clickHandler(scope, {$event: (event)});
-					scope.clicktimer = $timeout(function () {
-						delete scope.clicktimer;
-					}, 1, false);
-				});
-			});
-
-			// Hack for iOS Safari's benefit. It goes searching for onclick handlers and is liable to click
-			// something else nearby.
-			element.onclick = function (event) {
-			};
-		};
-	}]);
+if (ionic.Platform.isWindowsPhone()) {
+	$document.find('button').attr('data-tap-disabled', true);
 }
