466a6aa09e4ff316e05d3b012f649f952b20f044
Eleazar Morelli <gemorelli@gmail.com>
2015-07-09 03:18:38 -0300

    Refactor: Cleaning menu bloat

--- www/menu/menu.controller.js
+++ www/menu/menu.controller.js
@@ -1,6 +1,6 @@
 angular
 		.module('CriticalRoll')
-		.controller('MenuController', function ($scope, CardService, PopoverService) {
+		.controller('MenuController', function ($scope, $cordovaSocialSharing, CardService, PopoverService) {
 
 			$scope.showOption = function (option) {
 				var cardDrawn = {};
@@ -8,5 +8,15 @@ angular
 				CardService.placeCard(cardDrawn);
 				PopoverService.close();
 			};
+
+			$scope.share = function () {
+				PopoverService.close();
+				$cordovaSocialSharing.share(
+						$translate.instant('SOCIAL-SHARING-MESSAGE'),
+						$translate.instant('SOCIAL-SHARING-SUBJECT'),
+						null,
+						"https://play.google.com/store/apps/details?id=com.vanillaicecreamstudio.criticalroll"
+				);
+			}
 
 		});
