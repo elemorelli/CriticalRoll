6483715fefec692eed239000ff564dc759c4fba0
Eleazar Morelli <gemorelli@gmail.com>
2015-09-25 02:05:09 -0300

    Cleaning old langstrings

--- www/i18n/language.service.js
+++ www/i18n/language.service.js
@@ -12,42 +12,39 @@ angular.module('CriticalRoll').service('LanguageService', function ($translate) 
 }).config(['$translateProvider', function ($translateProvider) {
 
 	$translateProvider.useStaticFilesLoader(
-			{
-				files: [{
-					prefix: 'i18n/',
-					suffix: '/app.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/contribute.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/popover.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/critical-bludgeoning.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/critical-magic.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/critical-piercing.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/critical-slashing.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/fumble-magic.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/fumble-melee.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/fumble-natural.json'
-				}, {
-					prefix: 'i18n/',
-					suffix: '/fumble-ranged.json'
-				}]
-			}
+		{
+			files: [{
+				prefix: 'i18n/',
+				suffix: '/app.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/popover.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/critical-bludgeoning.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/critical-magic.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/critical-piercing.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/critical-slashing.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/fumble-magic.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/fumble-melee.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/fumble-natural.json'
+			}, {
+				prefix: 'i18n/',
+				suffix: '/fumble-ranged.json'
+			}]
+		}
 	);
 
 	$translateProvider.preferredLanguage(window.localStorage['language'] || 'en');
