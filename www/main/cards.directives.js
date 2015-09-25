2985d49dc2e20fc895f8ecd29761939395673196
Eleazar Morelli <gemorelli@gmail.com>
2015-09-25 01:54:15 -0300

    Better implementation of card's text using filters and a compile directive

--- www/main/cards.directives.js
+++ www/main/cards.directives.js
@@ -1,36 +1,31 @@
-angular.module('CriticalRoll')
-		.directive('card', function () {
-			return {
-				restrict: 'E',
-				templateUrl: 'main/card.html'
+angular.module('CriticalRoll').directive('card', function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'main/card.html'
+	}
+}).filter('hhh', function ($sce) {
+	return function (input) {
+		var replacedText = input.replace(/%([\w\s-]+)=([\w\s-]+)%/gi,
+			'<a class="popover-link" ng-click="openPopover($event, &apos;$1&apos;)">$2</a>');
+		return $sce.trustAsHtml(replacedText);
+	}
+}).directive('compileTemplate', function ($compile, $parse) {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attr) {
+			var parsed = $parse(attr.ngBindHtml);
+
+			function getStringValue() {
+				return (parsed(scope) || '').toString();
 			}
-		})
-		.directive('cardTitle', function ($compile, $translate, $sce) {
-			return {
-				restrict: 'E',
-				replace: true,
-				link: function (scope, element, attr) {
-					$translate(attr.content).then(function (translatedText) {
-						element.html(translatedText);
-						$compile(element.contents())(scope);
-					});
-				}
+
+			function compile() {
+				//The -9999 makes it skip directives so that we do not recompile ourselves
+				$compile(element, null, -9999)(scope);
 			}
-		})
-		.directive('cardText', function ($compile, $translate, $sce) {
-			return {
-				restrict: 'E',
-				replace: true,
-				link: function (scope, element, attr) {
-					scope.$watch(attr.content, function (translateKey) {
-						$translate(translateKey).then(function (translatedText) {
-							var replacedText = translatedText.replace(/%([\w\s-]+)=([\w\s-]+)%/gi,
-									'<a class="popover-link" ng-click="openPopover($event, &apos;$1&apos;)">$2</a>');
-							var parsedText = $sce.trustAsHtml(replacedText);
-							element.html(parsedText);
-							$compile(element.contents())(scope);
-						});
-					}, true);
-				}
-			}
-		});
+
+			//Recompile if the template changes
+			scope.$watch(getStringValue, compile);
+		}
+	}
+});
