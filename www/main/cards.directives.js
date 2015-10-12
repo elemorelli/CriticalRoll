a279622838b45525a100a761fcabaaef86ed4333
Eleazar Morelli <gemorelli@gmail.com>
2015-10-12 05:03:46 -0300

    Fixing more spanish data

--- www/main/cards.directives.js
+++ www/main/cards.directives.js
@@ -5,7 +5,7 @@ angular.module('CriticalRoll').directive('card', function () {
 	}
 }).filter('parseHTML', function ($sce) {
 	return function (input) {
-		var replacedText = input.replace(/%([\w\s-]+)=([\w\sñ-]+)%/gi,
+		var replacedText = input.replace(/%([\w\s-]+)=([\w\sñáéíóú-]+)%/gi,
 			'<a class="ruletip-link" ng-click="openRuletip(&apos;$1&apos;)">$2</a>');
 		return $sce.trustAsHtml(replacedText);
 	}
