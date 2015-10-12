0114c2f76f155f0d0dce6c32e3201cc2f90b0940
Eleazar Morelli <gemorelli@gmail.com>
2015-10-12 05:20:42 -0300

    Deleting card as a directive and moving language filter+directive into it's folder

---
+++ www/i18n/language.directive.js
@@ -0,0 +1,20 @@
+angular.module('CriticalRoll').directive('compileTemplate', function ($compile, $parse) {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attr) {
+			var parsed = $parse(attr.ngBindHtml);
+
+			function getStringValue() {
+				return (parsed(scope) || '').toString();
+			}
+
+			function compile() {
+				//The -9999 makes it skip directives so that we do not recompile ourselves
+				$compile(element, null, -9999)(scope);
+			}
+
+			//Recompile if the template changes
+			scope.$watch(getStringValue, compile);
+		}
+	}
+});
