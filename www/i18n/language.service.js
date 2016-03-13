bc1dea6017faf00fc59704553aef716198bfd1c6
Eleazar Morelli <gemorelli@gmail.com>
2016-03-12 21:24:45 -0300

    Refactor: Moving Language provider into another file

--- www/i18n/language.service.js
+++ www/i18n/language.service.js
@@ -3,8 +3,7 @@
 
     angular
         .module('CriticalRoll')
-        .service('LanguageService', LanguageService)
-        .config(['$translateProvider', translateProvider]);
+        .service('LanguageService', LanguageService);
 
     function LanguageService($translate) {
 
@@ -13,63 +12,6 @@
         function loadLanguage(system, language) {
             $translate.use(system + '-' + language);
         }
-    }
-
-    function translateProvider($translateProvider) {
-
-        var i18nPath = 'i18n/';
-
-        $translateProvider.useStaticFilesLoader({
-                files: [{
-                    prefix: i18nPath,
-                    suffix: '/app.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/ruletips.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/help.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/settings.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/critical-bludgeoning.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/critical-magic.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/critical-piercing.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/critical-slashing.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/fumble-magic.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/fumble-melee.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/fumble-natural.json'
-                }, {
-                    prefix: i18nPath,
-                    suffix: '/fumble-ranged.json'
-                }]
-            }
-        );
-
-        var settings = JSON.parse(window.localStorage.settings ? window.localStorage.settings : "{}");
-
-        if (!settings.system)
-            settings.system = 'pfrpg';
-
-        if (!settings.language)
-            settings.language = 'en';
-
-        $translateProvider.preferredLanguage(settings.system + '-' + settings.language);
-        $translateProvider.fallbackLanguage('pfrpg-en');
     }
 
 })();
