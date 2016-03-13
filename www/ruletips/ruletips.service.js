fac157a0744071b0daad105bddb65db5846981ac
Eleazar Morelli <gemorelli@gmail.com>
2016-03-13 00:58:12 -0300

    Placeholder for DnD 5th edition

--- www/ruletips/ruletips.service.js
+++ www/ruletips/ruletips.service.js
@@ -6,7 +6,7 @@
         .module('CriticalRoll')
         .service('RuletipsService', RuletipsService);
 
-    function RuletipsService($ionicPopup, $filter) {
+    function RuletipsService($ionicPopup, $filter, SettingsService) {
 
         this.ruletips = ruletips();
 
@@ -21,68 +21,58 @@
         }
 
         function ruletips() {
-            return [
-                {
-                    "title": "RULETIP-SECTION-DAMAGE",
-                    "rules": [
-                        "NONLETHAL-DAMAGE",
-                        "OBJECT-DAMAGE",
-                        "STATUS-NEGATIVE-LEVEL"
-                    ]
-                },
-                {
-                    "title": "RULETIP-SECTION-SAVES",
-                    "rules": [
-                        "UNTIL-HEALED",
-                        "SAVE-CRITICAL",
-                        "SAVE-FUMBLE"
-                    ]
-                },
-                {
-                    "title": "RULETIP-SECTION-ABILITY",
-                    "rules": [
-                        "ABILITY-DAMAGE",
-                        "ABILITY-DRAIN",
-                        "ABILITY-STR",
-                        "ABILITY-DEX",
-                        "ABILITY-CON",
-                        "ABILITY-INT",
-                        "ABILITY-WIS",
-                        "ABILITY-CHA"
-                    ]
-                },
-                {
-                    "title": "RULETIP-SECTION-STATUS",
-                    "rules": [
-                        "STATUS-BLEED",
-                        "STATUS-BLINDED",
-                        "STATUS-CONFUSED",
-                        "STATUS-DAZED",
-                        "STATUS-DAZZLED",
-                        "STATUS-DEAFENED",
-                        "STATUS-DYING",
-                        "STATUS-ENTANGLED",
-                        "STATUS-EXHAUSTED",
-                        "STATUS-FATIGUED",
-                        "STATUS-FLAT-FOOTED",
-                        "STATUS-FRIGHTENED",
-                        "STATUS-HELPLESS",
-                        "STATUS-INCORPOREAL",
-                        "STATUS-INVISIBLE",
-                        "STATUS-NAUSEATED",
-                        "STATUS-PANICKED",
-                        "STATUS-PARALYZED",
-                        "STATUS-PETRIFIED",
-                        "STATUS-PRONE",
-                        "STATUS-SHAKEN",
-                        "STATUS-SICKENED",
-                        "STATUS-STABLE",
-                        "STATUS-STAGGERED",
-                        "STATUS-STUNNED",
-                        "STATUS-UNCONSCIOUS"
-                    ]
-                }
-            ];
+
+            if (SettingsService.values.system == "dnd5") {
+                return [
+                    {
+                        "title": "RULETIP-SECTION-DAMAGE",
+                        "rules": ["NONLETHAL-DAMAGE", "OBJECT-DAMAGE", "STATUS-NEGATIVE-LEVEL"]
+                    },
+                    {
+                        "title": "RULETIP-SECTION-SAVES",
+                        "rules": ["UNTIL-HEALED", "SAVE-CRITICAL", "SAVE-FUMBLE"]
+                    },
+                    {
+                        "title": "RULETIP-SECTION-ABILITY",
+                        "rules": ["ABILITY-DAMAGE", "ABILITY-DRAIN", "ABILITY-STR", "ABILITY-DEX", "ABILITY-CON",
+                            "ABILITY-INT", "ABILITY-WIS", "ABILITY-CHA"]
+                    },
+                    {
+                        "title": "RULETIP-SECTION-STATUS",
+                        "rules": ["STATUS-BLEED", "STATUS-BLINDED", "STATUS-CONFUSED", "STATUS-DAZED", "STATUS-DAZZLED",
+                            "STATUS-DEAFENED", "STATUS-DYING", "STATUS-ENTANGLED", "STATUS-EXHAUSTED",
+                            "STATUS-FATIGUED", "STATUS-FLAT-FOOTED", "STATUS-FRIGHTENED", "STATUS-HELPLESS",
+                            "STATUS-INCORPOREAL", "STATUS-INVISIBLE", "STATUS-NAUSEATED", "STATUS-PANICKED",
+                            "STATUS-PARALYZED", "STATUS-PETRIFIED", "STATUS-PRONE", "STATUS-SHAKEN", "STATUS-SICKENED",
+                            "STATUS-STABLE", "STATUS-STAGGERED", "STATUS-STUNNED", "STATUS-UNCONSCIOUS"]
+                    }
+                ];
+            } else {
+                return [
+                    {
+                        "title": "RULETIP-SECTION-DAMAGE",
+                        "rules": ["NONLETHAL-DAMAGE", "OBJECT-DAMAGE", "STATUS-NEGATIVE-LEVEL"]
+                    },
+                    {
+                        "title": "RULETIP-SECTION-SAVES",
+                        "rules": ["UNTIL-HEALED", "SAVE-CRITICAL", "SAVE-FUMBLE"]
+                    },
+                    {
+                        "title": "RULETIP-SECTION-ABILITY",
+                        "rules": ["ABILITY-DAMAGE", "ABILITY-DRAIN", "ABILITY-STR", "ABILITY-DEX", "ABILITY-CON",
+                            "ABILITY-INT", "ABILITY-WIS", "ABILITY-CHA"]
+                    },
+                    {
+                        "title": "RULETIP-SECTION-STATUS",
+                        "rules": ["STATUS-BLEED", "STATUS-BLINDED", "STATUS-CONFUSED", "STATUS-DAZED", "STATUS-DAZZLED",
+                            "STATUS-DEAFENED", "STATUS-DYING", "STATUS-ENTANGLED", "STATUS-EXHAUSTED",
+                            "STATUS-FATIGUED", "STATUS-FLAT-FOOTED", "STATUS-FRIGHTENED", "STATUS-HELPLESS",
+                            "STATUS-INCORPOREAL", "STATUS-INVISIBLE", "STATUS-NAUSEATED", "STATUS-PANICKED",
+                            "STATUS-PARALYZED", "STATUS-PETRIFIED", "STATUS-PRONE", "STATUS-SHAKEN", "STATUS-SICKENED",
+                            "STATUS-STABLE", "STATUS-STAGGERED", "STATUS-STUNNED", "STATUS-UNCONSCIOUS"]
+                    }
+                ];
+            }
         }
     }
 
