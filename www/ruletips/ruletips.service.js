214d4c1eaf45d491c5c00c880e6011e423457193
Eleazar Morelli <gemorelli@gmail.com>
2016-03-13 14:47:34 -0300

    Adding 5th edition rules

--- www/ruletips/ruletips.service.js
+++ www/ruletips/ruletips.service.js
@@ -1,79 +1,77 @@
 (function () {
-    'use strict';
+  'use strict';
 
 
-    angular
-        .module('CriticalRoll')
-        .service('RuletipsService', RuletipsService);
+  angular
+    .module('CriticalRoll')
+    .service('RuletipsService', RuletipsService);
 
-    function RuletipsService($ionicPopup, $filter, SettingsService) {
+  function RuletipsService($ionicPopup, $filter, SettingsService) {
 
-        this.ruletips = ruletips();
+    this.ruletips = ruletips();
 
-        this.openRuletip = openRuletip;
+    this.openRuletip = openRuletip;
 
-        function openRuletip(ruletipType) {
+    function openRuletip(ruletipType) {
 
-            $ionicPopup.alert({
-                title: $filter('translate')(ruletipType + "-TITLE"),
-                template: $filter('translate')(ruletipType + "-TEXT")
-            });
-        }
-
-        function ruletips() {
-
-            if (SettingsService.values.system == "dnd5") {
-                return [
-                    {
-                        "title": "RULETIP-SECTION-DAMAGE",
-                        "rules": ["NONLETHAL-DAMAGE", "OBJECT-DAMAGE", "STATUS-NEGATIVE-LEVEL"]
-                    },
-                    {
-                        "title": "RULETIP-SECTION-SAVES",
-                        "rules": ["UNTIL-HEALED", "SAVE-CRITICAL", "SAVE-FUMBLE"]
-                    },
-                    {
-                        "title": "RULETIP-SECTION-ABILITY",
-                        "rules": ["ABILITY-DAMAGE", "ABILITY-DRAIN", "ABILITY-STR", "ABILITY-DEX", "ABILITY-CON",
-                            "ABILITY-INT", "ABILITY-WIS", "ABILITY-CHA"]
-                    },
-                    {
-                        "title": "RULETIP-SECTION-STATUS",
-                        "rules": ["STATUS-BLEED", "STATUS-BLINDED", "STATUS-CONFUSED", "STATUS-DAZED", "STATUS-DAZZLED",
-                            "STATUS-DEAFENED", "STATUS-DYING", "STATUS-ENTANGLED", "STATUS-EXHAUSTED",
-                            "STATUS-FATIGUED", "STATUS-FLAT-FOOTED", "STATUS-FRIGHTENED", "STATUS-HELPLESS",
-                            "STATUS-INCORPOREAL", "STATUS-INVISIBLE", "STATUS-NAUSEATED", "STATUS-PANICKED",
-                            "STATUS-PARALYZED", "STATUS-PETRIFIED", "STATUS-PRONE", "STATUS-SHAKEN", "STATUS-SICKENED",
-                            "STATUS-STABLE", "STATUS-STAGGERED", "STATUS-STUNNED", "STATUS-UNCONSCIOUS"]
-                    }
-                ];
-            } else {
-                return [
-                    {
-                        "title": "RULETIP-SECTION-DAMAGE",
-                        "rules": ["NONLETHAL-DAMAGE", "OBJECT-DAMAGE", "STATUS-NEGATIVE-LEVEL"]
-                    },
-                    {
-                        "title": "RULETIP-SECTION-SAVES",
-                        "rules": ["UNTIL-HEALED", "SAVE-CRITICAL", "SAVE-FUMBLE"]
-                    },
-                    {
-                        "title": "RULETIP-SECTION-ABILITY",
-                        "rules": ["ABILITY-DAMAGE", "ABILITY-DRAIN", "ABILITY-STR", "ABILITY-DEX", "ABILITY-CON",
-                            "ABILITY-INT", "ABILITY-WIS", "ABILITY-CHA"]
-                    },
-                    {
-                        "title": "RULETIP-SECTION-STATUS",
-                        "rules": ["STATUS-BLEED", "STATUS-BLINDED", "STATUS-CONFUSED", "STATUS-DAZED", "STATUS-DAZZLED",
-                            "STATUS-DEAFENED", "STATUS-DYING", "STATUS-ENTANGLED", "STATUS-EXHAUSTED",
-                            "STATUS-FATIGUED", "STATUS-FLAT-FOOTED", "STATUS-FRIGHTENED", "STATUS-HELPLESS",
-                            "STATUS-INCORPOREAL", "STATUS-INVISIBLE", "STATUS-NAUSEATED", "STATUS-PANICKED",
-                            "STATUS-PARALYZED", "STATUS-PETRIFIED", "STATUS-PRONE", "STATUS-SHAKEN", "STATUS-SICKENED",
-                            "STATUS-STABLE", "STATUS-STAGGERED", "STATUS-STUNNED", "STATUS-UNCONSCIOUS"]
-                    }
-                ];
-            }
-        }
+      $ionicPopup.alert({
+        title: $filter('translate')(ruletipType + "-TITLE"),
+        template: $filter('translate')(ruletipType + "-TEXT")
+      });
     }
+
+    function ruletips() {
+
+      if (SettingsService.values.system == "dnd5") {
+        return [
+          {
+            "title": "RULETIP-SECTION-DAMAGE",
+            "rules": ["OBJECT-DAMAGE", "NONLETHAL-DAMAGE"]
+          },
+          {
+            "title": "RULETIP-SECTION-SAVES",
+            "rules": ["SAVE-CRITICAL", "SAVE-FUMBLE", "UNTIL-HEALED"]
+          },
+          {
+            "title": "RULETIP-SECTION-ABILITY",
+            "rules": ["ABILITY-REDUCTION", "ABILITY-STR", "ABILITY-DEX", "ABILITY-CON", "ABILITY-INT", "ABILITY-WIS",
+              "ABILITY-CHA"]
+          },
+          {
+            "title": "RULETIP-SECTION-STATUS",
+            "rules": ["STATUS-BLEED", "STATUS-BLINDED", "STATUS-CHARMED", "STATUS-CONFUSED", "STATUS-DEAFENED",
+              "STATUS-DYING", "STATUS-EXHAUSTED", "STATUS-FRIGHTENED", "STATUS-INCAPACITATED", "STATUS-INCORPOREAL",
+              "STATUS-INVISIBLE", "STATUS-NEGATIVE-LEVEL", "STATUS-PARALYZED", "STATUS-PETRIFIED", "STATUS-POISONED",
+              "STATUS-PRONE", "STATUS-RESTRAINED", "STATUS-STABLE", "STATUS-STUNNED", "STATUS-UNCONSCIOUS"]
+          }
+        ];
+      } else {
+        return [
+          {
+            "title": "RULETIP-SECTION-DAMAGE",
+            "rules": ["NONLETHAL-DAMAGE", "OBJECT-DAMAGE", "STATUS-NEGATIVE-LEVEL"]
+          },
+          {
+            "title": "RULETIP-SECTION-SAVES",
+            "rules": ["UNTIL-HEALED", "SAVE-CRITICAL", "SAVE-FUMBLE"]
+          },
+          {
+            "title": "RULETIP-SECTION-ABILITY",
+            "rules": ["ABILITY-DAMAGE", "ABILITY-DRAIN", "ABILITY-STR", "ABILITY-DEX", "ABILITY-CON",
+              "ABILITY-INT", "ABILITY-WIS", "ABILITY-CHA"]
+          },
+          {
+            "title": "RULETIP-SECTION-STATUS",
+            "rules": ["STATUS-BLEED", "STATUS-BLINDED", "STATUS-CONFUSED", "STATUS-DAZED", "STATUS-DAZZLED",
+              "STATUS-DEAFENED", "STATUS-DYING", "STATUS-ENTANGLED", "STATUS-EXHAUSTED", "STATUS-FATIGUED",
+              "STATUS-FLAT-FOOTED", "STATUS-FRIGHTENED", "STATUS-HELPLESS", "STATUS-INCORPOREAL", "STATUS-INVISIBLE",
+              "STATUS-NAUSEATED", "STATUS-PANICKED", "STATUS-PARALYZED", "STATUS-PETRIFIED", "STATUS-PRONE",
+              "STATUS-SHAKEN", "STATUS-SICKENED", "STATUS-STABLE", "STATUS-STAGGERED", "STATUS-STUNNED",
+              "STATUS-UNCONSCIOUS"]
+          }
+        ];
+      }
+    }
+  }
 
 })();
