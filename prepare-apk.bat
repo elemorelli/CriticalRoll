26c061cc457a8f8824b797b603b4407a65d65568
Eleazar Morelli <gemorelli@gmail.com>
2016-03-18 01:20:02 -0300

    Adding Amazon's assets and updating apk bat

--- prepare-apk.bat
+++ prepare-apk.bat
@@ -3,6 +3,6 @@ del android-release*.apk
 call ionic build --release android
 copy platforms\android\build\outputs\apk\android-release-unsigned.apk android-release-unsigned.apk
 call "%java_home%\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk gemorelli
-call "%android_home%\build-tools\22.0.1\zipalign.exe" -v 4 android-release-unsigned.apk android-release.apk
+call "%android_home%\build-tools\23.0.2\zipalign.exe" -v 4 android-release-unsigned.apk android-release.apk
 del android-release-unsigned.apk
 @echo on
